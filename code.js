3ee99c18bf268524f57bcff4e1ec383c7ee501c7
Louiefigz <figueroa.aaron@gmail.com>
2016-04-23 02:20:13 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function favIceCream(name){
+  return "I love " + name;
+}
+
+function shouting(sentence){
+  return sentence.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth(name){
+  if (typeof name === "undefined"){
+    name = true;
+  }
+  return name;
+}
